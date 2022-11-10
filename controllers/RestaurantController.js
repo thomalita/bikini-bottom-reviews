668945aa3146ace70685524434d962b2205154c3
Thomalita Burbank <thomalita37@gmail.com>
2022-11-10 14:04:41 -0700

    finished review routes - initial

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -17,3 +17,26 @@ const CreateRestaurant = async (req, res) => {
         throw error
     }
 }
+
+
+const UpdateRestaurant = async (req, res) => {
+    try {
+      const restaurant = await Restaurant.update(
+        { ...req.body },
+        { where: { id: req.params.restaurant_id }, returning: true }
+      )
+      res.send(post)
+    } catch (error) {
+      throw error
+    }
+}
+
+const DeleteRestaurant = async (req, res) => {
+    try {
+      await Restaurant.destroy({ where: { id: req.params.restaurant_id } })
+      res.send({ msg: 'Restaurant Deleted', payload: req.params.restaurant_id, status: 'Ok' })
+    } catch (error) {
+      throw error
+    }
+  }
+  
--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -2,5 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/ReviewController')
 
 Router.get('/', controller.GetAllReviews)
-
+Router.post('/', controller.CreateReview)
+Router.delete('/review_id', controller.DeleteReview)
+Router.put('/review_id', UpdateReview)
 module.exports = Router
