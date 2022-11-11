adc1b95c852cdf09cb176a15053d8eed6c21372b
Alaysia Veal <alaysiaveal@gmail.com>
2022-11-11 12:48:54 -0500

    added delete seed to seed file

--- seeders/20221111164144-users.js
+++ seeders/20221111164144-users.js
@@ -3,50 +3,44 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      'users',
-      [
-        {
-          profilePic:
-            'https://vignette.wikia.nocookie.net/unanything/images/6/64/Bubble_Bass.jpg/revision/latest?cb=20140223074623',
-          username: 'Bubble Bass',
-          email: 'bbass@pickles.com',
-          password: '1234'
-        },
-        {
-          profilePic: 'https://i.ytimg.com/vi/ghvNSzGTQOg/maxresdefault.jpg',
-          username: 'Tom',
-          email: 'tomm@chocolate.com',
-          password: '1234'
-        },
-        {
-          profilePic: 'https://ih0.redbubble.net/image.365479721.3624/flat,800x800,075,f.jpg',
-          username: 'Hash Slinging Slasher',
-          email: 'hash@slasher.com',
-          password: '1234'
-        },
-        {
-          profilePic: 'https://wallpapercave.com/wp/Ggoin8Y.jpg',
-          username: 'Squidward',
-          email: 'squid@tentacles.com',
-          password: '1234'
-        },
-        {  
-          profilePic: 'http://clipartmag.com/images/spongebob-plankton-cliparts-35.jpg',
-          username: 'Plankton',
-          email: 'plankton@chumbucket.com',
-          password: '1234'
+    await queryInterface.bulkInsert('users', [
+      {
+        profilePic:
+          'https://vignette.wikia.nocookie.net/unanything/images/6/64/Bubble_Bass.jpg/revision/latest?cb=20140223074623',
+        username: 'Bubble Bass',
+        email: 'bbass@pickles.com',
+        password: '1234'
+      },
+      {
+        profilePic: 'https://i.ytimg.com/vi/ghvNSzGTQOg/maxresdefault.jpg',
+        username: 'Tom',
+        email: 'tomm@chocolate.com',
+        password: '1234'
+      },
+      {
+        profilePic:
+          'https://ih0.redbubble.net/image.365479721.3624/flat,800x800,075,f.jpg',
+        username: 'Hash Slinging Slasher',
+        email: 'hash@slasher.com',
+        password: '1234'
+      },
+      {
+        profilePic: 'https://wallpapercave.com/wp/Ggoin8Y.jpg',
+        username: 'Squidward',
+        email: 'squid@tentacles.com',
+        password: '1234'
+      },
+      {
+        profilePic:
+          'http://clipartmag.com/images/spongebob-plankton-cliparts-35.jpg',
+        username: 'Plankton',
+        email: 'plankton@chumbucket.com',
+        password: '1234'
       }
-    ]
-    )
+    ])
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('users', null, {})
   }
 }
