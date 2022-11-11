b850c40d4207a38b8eb24552bc5055feef087640
Thomalita Burbank <thomalita37@gmail.com>
2022-11-11 08:34:09 -0700

    updated routes

--- routes/RestaurantRouter.js
+++ routes/RestaurantRouter.js
@@ -2,9 +2,9 @@ const Router = require('express').Router()
 const controller = require('../controllers/RestaurantController')
 
 Router.get('/', controller.GetAllRestaurants)
-Router.post('/restaurants', controller.CreateRestaurant)
-Router.put('/restaurants/:restaurant_id', controller.UpdateRestaurant)
-Router.delete('/restaurants/:restaurant_id', controller.DeleteRestaurant)
+Router.post('/', controller.CreateRestaurant)
+Router.put('/:restaurant_id', controller.UpdateRestaurant)
+Router.delete('/:restaurant_id', controller.DeleteRestaurant)
 
 
 
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,9 +1,9 @@
 const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
-Router.get('/users', controller.GetAllUsers)
-Router.post('/users', controller.CreateUser)
-Router.put('/users/:user_id', controller.UpdateUser)
-Router.delete('/users/:user_id', controller.DeleteUser)
+Router.get('/', controller.GetAllUsers)
+Router.post('/', controller.CreateUser)
+Router.put('/:user_id', controller.UpdateUser)
+Router.delete('/:user_id', controller.DeleteUser)
 
 module.exports = Router
