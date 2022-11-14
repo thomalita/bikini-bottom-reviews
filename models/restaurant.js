bb530f13cb1aeb5349c853f8121338a3f31264a3
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-11-14 10:43:26 -0600

    added image to model

--- models/restaurant.js
+++ models/restaurant.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: DataTypes.STRING,
       address: DataTypes.STRING,
+      image: DataTypes.STRING,
       menu: DataTypes.STRING
     },
     {
